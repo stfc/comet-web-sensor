3da86c97f6dc86df9a8ebac92974fbb642cb09aa
Chris Gregory <61797427+chris-d-gregory@users.noreply.github.com>
2020-07-15 15:58:25 +0100

    add timestamp to data

--- sensor.py
+++ sensor.py
@@ -1,6 +1,7 @@
 import urllib.request
 import urllib.error
 import xml.etree.ElementTree as ET
+import datetime
 
 
 class Sensor():
@@ -8,9 +9,13 @@ class Sensor():
     def __init__(self,parms):
         self._ip = parms['ip']
         self._name = parms['name']
-        self._parameters = ['Temperature', 'Relative humidity', 'Dew point', 'CO2 level']
+        self._data_fields = ['Time', 'Temperature', 'CO2 level']
         self._data_received = False
     
+    
+    def _generate_timestamp(self, format = "%m/%d/%Y %H:%M:%S"):
+        return datetime.datetime.now().strftime(format)
+
 
     def _read_xml_from_web(self):
         try:
@@ -33,19 +38,28 @@ class Sensor():
         return xml_data[0].text != "1"
 
 
-    def _make_csv_list_from_xml(self, xml_data, val_names):
-        if not self._data_received:
-            data_list = ['connection?'] * len(val_names)
-        elif not self._xml_is_valid(xml_data):
-            data_list = ['invalid'] * len(val_names)
-        else:
-            data = {child.findtext('name'):child.findtext('aval') for child in xml_data if child.findall("unit") }
-            data_list = [data[v] for v in val_names]
-        return ','.join(data_list) 
-
-    
     def _get_latest_data(self):
-        return self._make_csv_list_from_xml(self.xml_data, self._parameters)
+        xml = self._read_xml_from_web()
+        if not self._data_received:
+            data = self._sub_data_with_error('connection')
+        elif not self._xml_is_valid(xml):
+            data = self._sub_data_with_error('invalid')
+        else:
+            data = {child.findtext('name'):child.findtext('aval') for child in xml if child.findall("unit") }
+            data = self._add_timestamp_if_required(data)
+        return data 
+
+
+    def _sub_data_with_error(self, reason):
+        err_dict = {field:reason for field in self._data_fields}
+        err_dict = self._add_timestamp_if_required(err_dict)
+        return err_dict
+    
+
+    def _add_timestamp_if_required(self, data_dictionary):
+            if 'Time' in self._data_fields:
+                data_dictionary['Time'] = self._generate_timestamp()
+            return data_dictionary
 
 
     @property
@@ -54,23 +68,30 @@ class Sensor():
       
     
     @property
-    def latest_data(self):
-        return self._get_latest_data() + '\n'
+    def latest_csv_data(self):
+        data_list = [self._get_latest_data()[df] for df in self._data_fields]
+        return ','.join(data_list) + '\n'
 
-    
+
     @property
-    def requested_data(self):
-        return ','.join(self._parameters) + '\n'
+    def latest_data(self):
+        return self._get_latest_data()
     
 
-    @requested_data.setter
-    def requested_data(self, req):
-        self._parameters = req
+    @property
+    def data_fields(self):
+        return ','.join(self._data_fields) + '\n'
+    
+
+    @data_fields.setter
+    def data_fields(self, fields):
+        self._data_fields = fields
 
 
     @property
     def ip(self):
         return self._ip.replace('.','_')
+
 
     @ip.setter
     def ip(self, ip):
