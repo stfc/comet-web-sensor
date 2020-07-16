4ef847aab37ee56e52a93a00f4a44d7e8e165e02
Chris Gregory <61797427+chris-d-gregory@users.noreply.github.com>
2020-07-16 09:24:44 +0100

    replace time.sleep with call to twisted

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os, datetime, time
 from pathlib import Path
 from sensor import Sensor
+from twisted.internet import task, reactor
 
 
 def get_today():
@@ -13,12 +14,12 @@ def make_dir_if_needed(filename):
         os.makedirs(directory)
 
 
-def generate_filename(sensor):
-    return os.getcwd() + os.sep + get_today() + os.sep + get_today() + '_' + sensor.name + '.csv'
+def generate_filename(sensor, root = os.getcwd()):
+    return root + os.sep + get_today() + os.sep + get_today() + '_' + sensor.ip + '.csv'
 
 
-def make_csv_file_if_necessary(sensor):
-    filename = generate_filename(sensor)
+def make_csv_file_if_necessary(sensor, root = os.getcwd()):
+    filename = generate_filename(sensor, root)
     if not Path(filename).is_file(): 
         make_dir_if_needed(filename)
         with open(filename, 'w') as f:
@@ -28,27 +29,32 @@ def make_csv_file_if_necessary(sensor):
 
 if __name__ == "__main__":
 
-    sensor_parms = [{'ip':'130.246.68.74', 'name':'Room1'},
-                    {'ip':'130.246.68.75', 'name':'Room2'},
-                    {'ip':'130.246.68.81', 'name':'Room3'},
-                    {'ip':'130.246.68.82', 'name':'Room4'},
-                    {'ip':'130.246.68.87', 'name':'Room5'},
-                    {'ip':'130.246.68.88', 'name':'Room6'},
-                    {'ip':'130.246.68.90', 'name':'Room7'},
-                    {'ip':'130.246.68.91', 'name':'Room8'},
-                    {'ip':'130.246.68.92', 'name':'Room9'},
-                    {'ip':'130.246.68.94', 'name':'Room10'}]
+    sensor_parms = [{'ip':'130.246.68.74', 'name':'Sensor1'},
+                    {'ip':'130.246.68.75', 'name':'Sensor2'},
+                    {'ip':'130.246.68.81', 'name':'Sensor3'},
+                    {'ip':'130.246.68.82', 'name':'Sensor4'},
+                    {'ip':'130.246.68.87', 'name':'Sensor5'},
+                    {'ip':'130.246.68.88', 'name':'Sensor6'},
+                    {'ip':'130.246.68.90', 'name':'Sensor7'},
+                    {'ip':'130.246.68.91', 'name':'Sensor8'},
+                    {'ip':'130.246.68.92', 'name':'Sensor9'},
+                    {'ip':'130.246.68.94', 'name':'Sensor10'}]
 
     sensors = [Sensor(parms) for parms in sensor_parms]
 
+
     for sensor in sensors:
         sensor.data_fields = ['Time', 'Temperature', 'Relative humidity', 'Dew point', 'CO2 level']
-
-    interval = 60   
     
-    while True:
+    
+    def get_data():
         for sensor in sensors:
-            csv_file = make_csv_file_if_necessary(sensor)
+            csv_file = make_csv_file_if_necessary(sensor, root='/home/jqg93617')
             with open(csv_file, 'a') as f:
                 f.write(sensor.latest_csv_data)
-        time.sleep(interval)
+
+
+    interval = 60.0 
+    loop = task.LoopingCall(get_data)
+    loop.start(interval)
+    reactor.run()
