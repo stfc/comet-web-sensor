8ff076e88932504ef02a844234294e537dcf13ca
Chris Gregory <61797427+chris-d-gregory@users.noreply.github.com>
2020-07-20 10:47:34 +0100

    add dropdown to choose graph type

--- server-ui.py
+++ server-ui.py
@@ -19,22 +19,44 @@ df = pd.read_csv('./20200716/20200716_sensors.csv')
 df = df.replace('connection','0')
 df = df.astype({'Temperature':'float', 'Relative humidity':'float', 'Dew point':'float', 'CO2 level': 'float', 'Time':'datetime64[ns]'})
 
-parameter = 'CO2 level'
+#parameter = 'CO2 level'
 
-fig = go.Figure()
-for key, grp in df.groupby(['ip']):
-    fig.add_scatter(x=grp['Time'], y=grp[parameter], name=key, mode='lines + markers')
+# fig = go.Figure()
+# for key, grp in df.groupby(['ip']):
+#     fig.add_scatter(x=grp['Time'], y=grp[parameter], name=key, mode='lines + markers')
 
-fig.layout
+
 
 app.layout = html.Div(children=[
 
+    html.H1(id='plot-title'),
 
     dcc.Graph(
-        id='scatter',
-        figure=fig
+        id='data-plot'
+    ),
+
+    dcc.Dropdown(
+        id='parameter-picker',
+        options=[
+            {'label': 'CO2 level', 'value': 'CO2 level'},
+            {'label': 'Temperature', 'value': 'Temperature'},
+            {'label': 'Dew point', 'value': 'Dew point'},
+            {'label': 'Humidity', 'value': 'Relative humidity'}
+        ],
+        value='CO2 level'
     ),
 ])
+
+@app.callback(
+    [Output('data-plot', 'figure'),
+    Output('plot-title', 'children')],
+    [Input('parameter-picker', 'value')])
+def update_output(value):
+
+    fig = go.Figure()
+    for key, grp in df.groupby(['ip']):
+        fig.add_scatter(x=grp['Time'], y=grp[value], name=key, mode='lines + markers')
+    return fig, value
 
 if __name__ == '__main__':
     app.run_server(debug=True)
