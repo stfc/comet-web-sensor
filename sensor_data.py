f3b4f2d1be3a5b6a305120cd6c6184433ee72326
Chris Gregory <61797427+chris-d-gregory@users.noreply.github.com>
2020-07-28 16:21:29 +0100

    move configuration to config file

--- sensor_data.py
+++ sensor_data.py
@@ -1,93 +1,18 @@
 #!/usr/bin/env python3
-import os, datetime, time, atexit
-from pathlib import Path
-from sensor import Sensor
-
-
-
-def get_today():
-    return datetime.date.today().strftime("%Y%m%d")
-    
-
-def make_dir_if_needed(filename):
-    directory, _ = os.path.split(filename)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-
-def generate_sensor_filename(sensor, root = os.getcwd()):
-    return root + os.sep + get_today() + os.sep + get_today() + '_' + sensor.ip + '.csv'
-
-
-def generate_todays_filename(root = os.getcwd()):
-    return root + os.sep + get_today() + '_sensors' + '.csv'
-
-
-def make_sensor_csv_file_if_necessary(sensor, root = os.getcwd()):
-    filename = generate_sensor_filename(sensor, root)
-    if not Path(filename).is_file(): 
-        make_dir_if_needed(filename)
-        with open(filename, 'w') as f:
-            f.write(sensor.data_fields)
-    return filename
-
-def make_todays_csv_file_if_necessary(fields, root = os.getcwd()):
-    filename = generate_todays_filename(root)
-    if not Path(filename).is_file(): 
-        make_dir_if_needed(filename)
-        with open(filename, 'w') as f:
-            f.write(fields)
-    return filename
+from sensor_data_reader import SensorDataReader
 
 
 if __name__ == "__main__":
-
-    sensor_parms = [{'ip':'130.246.68.74', 'name':'R1 Mech Zund'},
-                    {'ip':'130.246.68.75', 'name':'R1 T-fab office'},
-                    {'ip':'130.246.68.81', 'name':'R1 Gemini office'},
-                    {'ip':'130.246.68.82', 'name':'R1 Mech CNC'},
-                    {'ip':'130.246.68.87', 'name':'RCaH1'},
-                    {'ip':'130.246.68.88', 'name':'R1 Mech Assy'},
-                    {'ip':'130.246.68.90', 'name':'R1 Directorate'},
-                    {'ip':'130.246.68.91', 'name':'RCaH2'},
-                    {'ip':'130.246.68.92', 'name':'R1 Mech office'},
-                    {'ip':'130.246.68.94', 'name':'Mobile unit'},
-                    {'ip':'130.246.68.111', 'name':'R1 VCR'},
-                    {'ip':'130.246.68.112', 'name':'R1 Mech Manual'},
-                    {'ip':'130.246.68.114', 'name':'R1 CALTA offices'},
-                    {'ip':'130.246.68.121', 'name':'R1 Theory offices'},
-                    {'ip':'130.246.68.111', 'name':'R1 Electrical offices'}]
-
-
-    sensors = [Sensor(parms) for parms in sensor_parms]
-
-    fields = ['Temperature', 'Relative humidity', 'Dew point', 'CO2 level']
-
-    interval = 60
-
-    for sensor in sensors:
-        sensor.data_fields = fields
-        sensor.start_data_collection(interval=interval)
+    sdr = SensorDataReader('config.ini')
+    sdr.start()
     
-          
-    def generate_timestamp(format = "%m/%d/%Y %H:%M:%S"):
-        return datetime.datetime.now().strftime(format)
+
+
+    
     
     
 
-    if __name__ == '__main__':
-        while True:
-            # Stuff for Steve Blake - one file per sensor per day
-            for sensor in sensors:
-                csv_file = make_sensor_csv_file_if_necessary(sensor, root='/opt/sensor_data/sensor_data')
-                with open(csv_file, 'a') as f:
-                    f.write(sensor.latest_csv_data)
-                
-            # Stuff for Dash
-                csv_file_dash = make_todays_csv_file_if_necessary('ip,name,' + ','.join(fields)+'\n', root='/opt/sensor_data/dash')
-                with open(csv_file_dash, 'a') as f:
-                    f.write(sensor.ip+','+sensor.name+','+sensor.latest_csv_data)
-            time.sleep(interval)
+    
             
             
 
---
+++ sensor_data_reader.py
@@ -0,0 +1,57 @@
+import os, datetime, time
+from configparser import ConfigParser
+from pathlib import Path
+from sensor import Sensor
+
+
+class SensorDataReader():
+    def __init__(self, config_file='config.ini'):
+        self._read_config_file(config_file)
+        self._sensors = [Sensor(d) for d in self._sensors_details]
+
+
+    def _read_config_file(self, config_file):
+        cp = ConfigParser()
+        cp.read(config_file)
+        self._sensors_details = [{'ip':i[0], 'name':i[1]} for i in cp['sensors'].items()]
+        self._sample_interval = cp.getfloat('settings', 'interval in seconds')
+        self._data_file_location = cp.get('settings', 'data file location')
+
+
+    def _get_today(self):
+        return datetime.date.today().strftime("%Y%m%d")
+    
+
+    def _make_dir_if_needed(self, filename):
+        directory, _ = os.path.split(filename)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+
+    def _generate_todays_filename(self):
+        return self._data_file_location + os.sep + self._get_today() + '_sensors.csv'
+
+
+    def _make_todays_csv_file_if_necessary(self):
+        filename = self._generate_todays_filename()
+        if not Path(filename).is_file():
+            first_line_in_file = ('ip,name,Time,Temperature,Relative humidity,Dew point,CO2 level\n') 
+            self._make_dir_if_needed(filename)
+            with open(filename, 'w') as f:
+                f.write(first_line_in_file)
+        return filename
+
+            
+    def _start_sensors(self):
+        for sensor in self._sensors:
+            sensor.start_data_collection(self._sample_interval)
+
+    
+    def start(self):
+        self._start_sensors()
+        while True:
+            csv_file = self._make_todays_csv_file_if_necessary()
+            with open(csv_file, 'a') as f:
+                for s in self._sensors:
+                    f.write(s.ip+','+s.name+','+s.latest_csv_data)
+            time.sleep(self._sample_interval)
